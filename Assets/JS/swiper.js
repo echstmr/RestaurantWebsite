7e1e207a3d53325b1ea4bfb32fcedd7091f03696
geo <153982313+echstmr@users.noreply.github.com>
2024-11-21 10:34:45 +0800

    Add files via upload

---
+++ Assets/JS/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    speed: 800, 
+  });
+  
